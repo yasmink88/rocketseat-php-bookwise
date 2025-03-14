9d4cb2628d1431c718b5dbf2ad81a66add64b119
yasmink88 <yasmin.khalaf.a@gmail.com>
2025-03-13 21:56:22 -0300

    First commit

---
+++ book.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookWise</title>
+</head>
+<body class="bg-zinc-950 text-gray-300">
+    <header class="bg-stone-900 text-green-400">
+        <nav class="mx-auto max-w-screen-lg flex justify-between  px-8 py-4">
+            <div class="logo font-bold text-xl text-gray-100">BookWise</div>
+            <ul class="flex gap-4">
+                <li><a href="/" class="hover:underline">Explore</a></li>
+                <li><a href="#" class="hover:underline">My books</a></li>
+            </ul>
+            <div class="login">
+                <a href="#" class="hover:underline">Login</a>
+            </div>
+        </nav>
+    </header>
+
+    <main class="mx-auto max-w-screen-lg px-8 py-4 flex flex-col gap-4 my-4">
+        <h1 class="hidden">Book Title</h1>
+
+    </main>
+    
+<script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+</body>
+</html>
